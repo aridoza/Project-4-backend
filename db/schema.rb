3629799e8337150124535288885c8ffea4dbd4ef
aridoza <aridoza85@gmail.com>
2016-05-11 10:48:27 -0400

    added postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160505161654) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "parkings", force: :cascade do |t|
     t.string   "date"
     t.string   "time"
